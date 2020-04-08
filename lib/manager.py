a2c9f7f3cc19b20155fb464005a78ae60aebd265
Valentin Liévin <valentin.lievin@gmail.com>
2020-04-08 10:19:55 +0200

    filelock path

--- lib/manager.py
+++ lib/manager.py
@@ -1,18 +1,32 @@
 import json
 import os
-
 from tinydb import TinyDB, Query  # pip install tinydb
 
 
 def open_db(logdir):
     _file = os.path.join(logdir, '.db.json')
+
+    # sometimes the file get corrupted with trailing null bytes (\0x00)
+    # remove them as an ugly and quick workaround that works in some cases
+    # sometimes it still fails inconsistently: this must be due to the shared
+    # file system. Using /nocbackup may solve the issue.
+    # with filelock.FileLock(get_filelock(logdir)):
+    #     with open(_file, 'r') as fp:
+    #         s = fp.read()
+    #
+    #     if s.find('\x00'):
+    #         print("# manager.open_db: removing null bytes.")
+    #         s = s.replace('\x00', '')
+    #         with open(_file, 'w') as fp:
+    #             fp.write(s)
+
     db = TinyDB(_file, indent=4)
     query = Query()
     return db.table('experiments'), query
 
 
 def get_filelock(abs_logdir):
-    return os.path.join(abs_logdir, ".db.json.lock")
+    return os.path.join(abs_logdir, ".db.json")
 
 def snapshot_dir(logdir):
     return os.path.join(logdir, '.lib_snapshot')
