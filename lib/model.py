113e2cf7718f8c2e05864ff72910f163e1d3da2f
Valentin Liévin <valentin.lievin@gmail.com>
2020-04-08 10:20:23 +0200

    added toy model

--- lib/model.py
+++ lib/model.py
@@ -82,6 +82,7 @@ class BaseVAE(Template):
         self.prior_dist = {'categorical': PseudoCategorical, 'normal': NormalFromLogits}[prior]
         if prior == 'normal':
             K = 2
+            kdim = 0
 
         # output distribution p(x|z)
         self.likelihood = likelihood
@@ -170,10 +171,9 @@ class BaseVAE(Template):
             usage = (z.sum(dim=0, keepdim=True)>0).float()
             usage = usage.mean(dim=(1,2,))
         else:
-            usage = None
+            usage = torch.zeros_like(Hp)
 
         return {'Hp': [Hp], 'usage': [usage]}
-
 
 class VAE(BaseVAE):
     """
@@ -244,6 +244,73 @@ class ConvVAE(BaseVAE):
         # unpad
         px_logits = torch.nn.functional.pad(px_logits, [-p for p in self.padding])
         return self.likelihood(logits=px_logits)
+
+
+
+class ToyVAE(Template):
+    """
+    A simple VAE model parametrized by MLPs
+    """
+
+    def __init__(self, xdim, N, K, hdim, **kwargs):
+        super().__init__()
+        D = xdim[0]
+        self.xdim = xdim
+        self.prior_dist = NormalFromLoc
+        self.likelihood = NormalFromLoc
+
+        self.q_mu = nn.Linear(D, D)
+        self.register_buffer("q_scale", 2/3 * torch.ones((1, D,)))
+        self.p_mu = nn.Parameter(torch.zeros((1, D,)))
+
+
+    def generate(self, z):
+        return self.likelihood(logits=z)
+
+
+    def forward(self, x, tau=0, zgrads=False, mc=1, iw=1, **kwargs):
+        qlogits =  self.q_mu(x)
+        qlogits.retain_grad()
+
+        if mc > 1 or iw > 1:
+            bs, *dims = qlogits.shape
+            qlogits_expanded = qlogits[:, None, None, :].expand(x.size(0), mc, iw, *dims).contiguous()
+            qlogits_expanded = qlogits_expanded.view(-1, *dims)
+
+        else:
+            qlogits_expanded = qlogits
+
+        qz = self.prior_dist(logits=qlogits_expanded, scale=self.q_scale)
+        z = qz.rsample()
+
+        if not zgrads:
+            z = z.detach()
+
+        pz = self.prior_dist(logits=self.p_mu)
+
+        px = self.generate(z)
+
+        diagnostics = self._get_diagnostics(z, qz, pz)
+
+        return {'px': px, 'z': [z], 'qz': [qz], 'pz': [pz], 'qlogits': [qlogits], **diagnostics}
+
+    def sample_from_prior(self, N):
+        prior = self.p_mu.expand(N, *self.xdim)
+        z = self.prior_dist(logits=prior).sample()
+        px = self.generate(z)
+        return {'px': px, 'z': z}
+
+
+    def _get_diagnostics(self, z, qz, pz):
+        Hp = batch_reduce(pz.entropy())
+        if isinstance(pz, PseudoCategorical):
+            usage = (z.sum(dim=0, keepdim=True)>0).float()
+            usage = usage.mean(dim=(1,2,))
+        else:
+            usage = torch.zeros_like(Hp)
+
+        return {'Hp': [Hp], 'usage': [usage]}
+
 
 
 class Stage(nn.Module):
