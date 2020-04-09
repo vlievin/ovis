grads: added percentiles report + fixed variance averaging

lib/gradients.py
@@ -4,7 +4,7 @@ from time import time
 import torch
 from tqdm import tqdm
 
-eps = 1e-18
+eps = 1e-15
 
 
 def covariance(x):
@@ -13,10 +13,11 @@ def covariance(x):
     return cov
 
 
-def _median(x):
+def _percentile(x, q=0.5):
+    assert q < 1
     x = x.view(-1)
-    k = int(0.5 * x.shape[0])
-    v, idx = x.kthvalue(k)
+    k = int(q * x.shape[0]) + 1
+    v, idx = x.kthvalue(k)  # indexing from 1..
     return v
 
 
@@ -61,33 +62,42 @@ class Variance():
         return (self.Ex2 - (self.Ex * self.Ex) / self.n) / (self.n - 1)
 
 
-def get_grads_from_qlogits(model, loss, output):
-    model.zero_grad()
-    loss.mean().backward(create_graph=True, retain_graph=True)
+def get_grads_from_tensor(model, loss, output, tensor_id):
+    assert tensor_id in output.keys(), f"Tensor_id = `{tensor_id}` not in model's output"
 
-    # get the logits of the variational distributions
-    q_logits = [p for i, p in enumerate(output['qlogits'])]
-    bs = q_logits[0].shape[0]
+    model.zero_grad()
+    loss.sum().backward(create_graph=True, retain_graph=True)
+
+    # get the tensor of interest
+    tensors = output[tensor_id] if isinstance(output[tensor_id], list) else output[tensor_id]
+    bs = tensors[0].shape[0]
     # get the gradients, flatten and concat across the feature dimension
-    gradients = torch.cat([p.grad.view(bs, -1) for p in q_logits], 1)
+    gradients = [p.grad for p in tensors]
+    assert not any(
+        [g is None for g in gradients]), f"{sum([int(g is None) for g in gradients])} tensors have no gradients." \
+                                         f"Use `tensor.retain_graph()` in your model to enable gradients."
+    gradients = torch.cat([g.view(bs, -1) for g in gradients], 1)
+
     # return each individual gradient
     for grads in gradients:
         yield grads
 
 
 def get_grads_from_parameters(model, loss, key_filter=''):
+    params = [p for k, p in model.named_parameters() if key_filter in k]
+    assert len(params) > 0, f"`No parameter matching the filter `{key_filter}`"
     for j, l in enumerate(loss):
         model.zero_grad()
         # backward individual gradients \nabla L[i]
         l.mean().backward(create_graph=True, retain_graph=True)
         # gather gradients for each parameter and concat such that each element across the dim 1 is a parameter
-        grads = [p.grad.view(-1) for k, p in model.named_parameters() if
-                 (p.grad is not None) and (key_filter in k)]
+        grads = [p.grad.view(-1) for p in params if p.grad is not None]
         # return each individual gradient
-        yield torch.cat(grads, 0)
+        grads = torch.cat(grads, 0)
+        yield grads
 
 
-def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=1000, seed=None, key_filter='', **config):
+def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=100, seed=None, key_filter='qlogits', true_grads=None, **config):
     """
     Compute the variance, magnitude and SNR of the gradients.
     """
@@ -95,10 +105,6 @@ def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=1000,
     n_individual_samples = n_samples * estimator.mc * estimator.iw
     effective_batch_size = max(1, batch_size // (estimator.mc * estimator.iw))
     iterations = n_samples // effective_batch_size + int(n_samples % effective_batch_size > 0)
-
-    print(f">>> Gradients: n_samples = {n_samples}, batch_size = {batch_size}, iw = {estimator.iw}")
-    print(
-        f">>> Gradients: n_individual_samples = {n_individual_samples}, effective_batch_size = {effective_batch_size}, iterations = {iterations}")
 
     _start = time()
 
@@ -111,6 +117,9 @@ def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=1000,
     grads_snr = Mean()
     grads_mean = Mean()
     grads_variance = Mean()
+    if true_grads is not None:
+        grads_dir = Mean()
+        true_grads = true_grads / true_grads.norm(p=2)
 
     with tqdm(total=x.shape[0] * iterations) as pbar:
 
@@ -119,8 +128,12 @@ def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=1000,
 
             grads_mean_i = Mean()
             grads_variance_i = Variance()
+            grads_dir_i = Mean() if true_grads is not None else None
 
             while grads_mean_i.n < n_samples:
+
+                remaining = n_samples - grads_mean_i.n
+                batch_size_i = min(remaining, effective_batch_size)
 
                 pbar.set_description(f"gradients analysis [x = {i} / {x.shape[0]}, n = {grads_mean_i.n} / {n_samples}]")
                 pbar.update(1)
@@ -129,13 +142,16 @@ def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=1000,
                 model.zero_grad()
 
                 # forward, backward to compute the gradients
-                loss, diagnostics, output = estimator(model, x_i, backward=False, **config)
+                loss, diagnostics, output = estimator(model, x_i[:batch_size_i], backward=False, **config)
 
                 # gather individual gradients
-                if 'qlogits' == key_filter:
-                    gradients = get_grads_from_qlogits(model, loss, output)
+                if 'tensor:' in key_filter:
+                    tensor_id = key_filter.replace("tensor:", "")
+                    gradients = get_grads_from_tensor(model, loss, output, tensor_id)
                 else:
                     gradients = get_grads_from_parameters(model, loss, key_filter=key_filter)
+
+                gradients = list(gradients)
 
                 # update statistics
                 for grads in gradients:
@@ -153,18 +169,16 @@ def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=1000,
             grads_variance_i = grads_variance_i()
             grads_mean_i = grads_mean_i()
 
-            # filter
-            mask = grads_variance_i > 0
-            grads_variance_i = grads_variance_i[mask]
-            grads_mean_i = grads_mean_i[mask]
-
             # compute signal-to-noise ratio. see `tighter variational bounds are not necessarily better` (eq. 4)
-            grads_snr_i = grads_mean_i.abs() / grads_variance_i ** (0.5)
+            grads_snr_i = grads_mean_i.abs() / (eps + grads_variance_i ** 0.5)
 
             # update global statistics
-            grads_mean.update(grads_variance_i)
+            grads_mean.update(grads_mean_i)
             grads_variance.update(grads_variance_i)
             grads_snr.update(grads_snr_i)
+            if true_grads is not None:
+                cosine_grads_i = (grads_mean_i * true_grads).sum() / grads_mean_i.norm(p=2)
+                grads_dir.update(cosine_grads_i)
 
     print(f">>> elapsed time = {time() - _start:.3f}")
 
@@ -175,12 +189,26 @@ def get_gradients_statistics(estimator, model, x, batch_size=32, n_samples=1000,
     grads_mean = grads_mean()
     grads_variance = grads_variance()
     grads_snr = grads_snr()
+    if true_grads is not None:
+        grads_dir = grads_dir()
 
     # print(f">> grads: iw = {estimator.iw}, elapsed time = {time() - _start:.3f}, snr = {snr.mean().log().item():.3f}, masked. snr {_mean(snr).log().item():.3f},  log_var = {_mean(variance).log().item():.3f}, Estimator = {type(estimator).__name__}")
 
     if seed is not None:
         torch.manual_seed(_seed)
 
+    # reduce fn
     _reduce = _mean
-    return {'variance': _reduce(grads_variance), 'magnitude': _reduce(grads_mean.abs()), 'snr': _reduce(grads_snr),
-            'keep_ratio': mask.float().sum() / mask.shape[0]}
+
+    return {'grads': {
+        'variance': _reduce(grads_variance),
+        'magnitude': _reduce(grads_mean.abs()),
+        'snr': _reduce(grads_snr),
+        'direction': _reduce(grads_dir) if true_grads is not None else 0.
+    },
+        'snr': {
+            'p25': _percentile(grads_snr, q=0.25), 'p50': _percentile(grads_snr, q=0.50),
+            'p75': _percentile(grads_snr, q=0.75), 'p5': _percentile(grads_snr, q=0.05),
+            'p95': _percentile(grads_snr, q=0.95), 'min': grads_snr.min(),
+            'max': grads_snr.max(), 'mean': grads_snr.mean()}
+    }

run.py
@@ -1,8 +1,8 @@
 import argparse
 import json
 import os
-import traceback
 import socket
+import traceback
 from shutil import rmtree
 
 import numpy as np
@@ -13,7 +13,6 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-
 from lib import VAE, Baseline, ConvVAE, ToyVAE
 from lib import VariationalInference
 from lib import get_datasets
@@ -22,7 +21,6 @@ from lib.gradients import get_gradients_statistics
 from lib.logging import sample_model, get_loggers, log_summary, save_model, load_model
 from lib.ops import training_step, test_step
 from lib.utils import notqdm
-from lib.analysis import analyse_control_variate
 
 _sep = os.get_terminal_size().columns * "-"
 
@@ -62,6 +60,7 @@ parser.add_argument('--iw', default=1, type=int, help='number of Importance-Weig
 # evaluation
 parser.add_argument('--eval_freq', default=10, type=int, help='frequency for the evaluation [test set + grads]')
 parser.add_argument('--iw_valid', default=100, type=int, help='number of Importance-Weighted samples for validation')
+parser.add_argument('--iw_test', default=100, type=int, help='number of Importance-Weighted samples for testing')
 # gradients analysis
 parser.add_argument('--grad_samples', default=100, type=int,
                     help='number of samples used to evaluate the variance.')
@@ -85,7 +84,6 @@ parser.add_argument('--norm', default='layernorm', type=str, help='normalization
 
 opt = parser.parse_args()
 
-
 print(f"## run.py: opt.root = {opt.root}")
 
 if opt.deterministic:
@@ -95,19 +93,20 @@ if opt.deterministic:
 if opt.silent:
     tqdm = notqdm
 
-
 # define conunterfactuals
 if len(opt.counterfactuals):
     counterfactual_estimators_ids = opt.counterfactuals.replace(" ", "").split(",")
+
 
     def _split(e):
         if "-iw" in e:
             arg = e.split("-")[-1]
             iw = eval(arg.replace("iw", ""))
-            e = e.replace(f"-iw{iw}" , "")
+            e = e.replace(f"-iw{iw}", "")
             return e, iw
         else:
             return e, opt.iw
+
 
     counterfactual_estimators, counterfactual_iw = zip(*[_split(e) for e in counterfactual_estimators_ids])
 else:
@@ -256,27 +255,30 @@ try:
             x_eval = next(iter(loader_train)).to(device)  # [:opt.grad_samples]
 
             # estimate the variance of the gradients
-            grad_args = {'seed': opt.seed, 'batch_size': opt.bs * opt.mc * opt.iw, 'n_samples:' : opt.grad_samples, 'key_filter': 'qlogits'}
-            # current model
-            summary_train["grads"] = get_gradients_statistics(estimator, model, x_eval, **grad_args,
-                                                                        **config)
+            grad_args = {'seed': opt.seed, 'batch_size': opt.bs * opt.mc * opt.iw, 'n_samples': opt.grad_samples,
+                         'key_filter': 'tensor:qlogits'}
+            # for the main estimator
+            grad_data = get_gradients_statistics(estimator, model, x_eval, **grad_args, **config)
+            summary_train.update(grad_data)
+            train_logger.info(f"{_exp_id} | grads | "
+                              f"snr = {grad_data.get('grads', {}).get('snr', 0.):.3E}, "
+                              f"variance = {grad_data.get('grads', {}).get('variance', 0.):.3E}, "
+                              f"magnitude = {grad_data.get('grads', {}).get('magnitude', 0.):.3E}")
 
-            grad_data = summary_train["grads"]
-            train_logger.info(f"{_exp_id} | grads | snr = {grad_data.get('snr', 0.):.3E}, variance = {grad_data.get('variance', 0.):.3E}, magnitude = {grad_data.get('magnitude', 0.):.3E}")
-
-            # analyse the control variate terms on the counterfactual estimators
+            # analyse the control variate terms on the counter-factual estimators
             # analyse_control_variate(x_eval, model, c_configs, c_estimators, counterfactual_estimators, writer_train, seed=opt.seed, global_step=global_step)
 
-            # counter factual estimation of other estimators
+            # counter-factual estimation of the other estimators
             for (c_writer, c_conf, c_est, c) in zip(counterfactual_writers, c_configs, c_estimators,
                                                     counterfactual_estimators):
                 grad_data = get_gradients_statistics(c_est, model, x_eval, **grad_args, **c_conf)
-                summary = Diagnostic({'grads': grad_data})
+                summary = Diagnostic(grad_data)
                 summary.log(c_writer, global_step)
                 train_logger.info(f" | counterfactual | {c:{max(map(len, counterfactual_estimators))}s} "
-                                  f"| snr = {grad_data.get('snr', 0.):.3f}, variance = {grad_data.get('variance', 0.):.3f}")
+                                  f"| snr = {grad_data.get('grads', {}).get('snr', 0.):.3f}, "
+                                  f"variance = {grad_data.get('grads', {}).get('variance', 0.):.3f}")
 
-            # valid epoch
+            # validation epoch
             with torch.no_grad():
                 model.eval()
                 agg_valid.initialize()
@@ -310,12 +312,14 @@ try:
                         param_group['lr'] = new_lr
                         base_logger.info(f"Reducing lr, group = {i} : {lr:.2E} -> {new_lr:.2E}")
 
-
     print("TESTING..", best_elbo)
     # testing step
     writer_test = SummaryWriter(os.path.join(logdir, 'test'))
     loader_test = DataLoader(dset_test, batch_size=2 * opt.bs, shuffle=True, num_workers=1)
     agg_test = Aggregator()
+
+    config_test = {'tau': 0, 'zgrads': False}
+    estimator_test = VariationalInference(mc=1, iw=opt.iw_test, sequential_computation=True)
 
     # load best model and run over the test set
     load_model(model, logdir)
@@ -323,7 +327,7 @@ try:
     agg_test.initialize()
     for x in tqdm(loader_test, desc=_exp_id + "-test"):
         x = x.to(device)
-        diagnostics = test_step(x, model, estimator_valid, **config)
+        diagnostics = test_step(x, model, estimator_test, **config_test)
         agg_test.update(diagnostics)
     summary_test = agg_test.data.to('cpu')
 
@@ -340,6 +344,7 @@ except KeyboardInterrupt:
     print("## KEYBOARD INTERRUPT")
     with open(os.path.join(logdir, "success.txt"), 'w') as f:
         f.write(f"Failed. Interrupted (keyboard).")
+
 
 except Exception as ex:
     print("## FAILED. Exception:")