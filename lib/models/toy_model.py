ec4c89d9edd63d56a34e0eeef54dcfb3513c2a29
Anderschri <47868390+Anderschri@users.noreply.github.com>
2020-04-27 23:53:00 +0200

    Update toy_model.py

--- lib/models/toy_model.py
+++ lib/models/toy_model.py
@@ -1,14 +1,12 @@
 from copy import copy
-
 from typing import Dict
 
 import torch
 from torch import nn, Tensor
-from torch.distributions import Bernoulli
+from torch.distributions import Bernoulli, Normal
 
-from .models import Template
+from .base import Template
 from ..distributions import *
-from ..layers import *
 from ..utils import *
 
 
@@ -16,24 +14,25 @@ class ToyModel(Template):
     """ Trivial model for the toy example from "Backpropagation Through the Void" paper
     https://arxiv.org/pdf/1711.00123.pdf
     using all estimators """
-    def __init__(self, num_latents, batch_size, logits = nn.Parameter(torch.zeros(1))): # , mc, iw, logits = nn.Parameter(torch.zeros(1))):
+    def __init__(self, xdim, N, K, hdim, **kwargs): # (self, num_latents, batch_size):
         super().__init__()
         self.device = "cuda:0" if torch.cuda.device_count() else "cpu"
-        self.qlogits = nn.Parameter(torch.zeros(num_latents, requires_grad=True, device=self.device), requires_grad=True)
-        self.num_latents = num_latents
-        self.batch_size = batch_size
-        self.device = "cuda:0" if torch.cuda.device_count() else "cpu"
+        self.num_latents = N
+        self.qlogits = nn.Parameter(torch.zeros(N, requires_grad=True, device=self.device), requires_grad=True)
+        self.prior_logits = torch.zeros_like(self.qlogits, device=self.device)
 
     def forward(self, x, tau=0, zgrads=False, mc=1, iw=1, **kwargs): # (self, mc=1, iw=1, **kwargs): # (self, *args, **kwargs) -> Dict[str, Tensor]: # (self, x: Tensor, **kwargs) -> Dict[str, Tensor]:
+        bs = x.shape[0]
+
         # q_theta(z | x)
         self.qlogits.retain_grad()
         qz = Bernoulli(logits=self.qlogits)
 
         # prior: p(z)
-        pz = Bernoulli(logits=torch.zeros_like(self.qlogits, device=self.device)) # Bernoulli(logits=self.logits)
+        pz = Bernoulli(logits=self.prior_logits)
 
         # sample posterior
-        u = torch.rand(self.batch_size * iw * mc, self.num_latents).to(self.device) # u ~ Unif[0,1]
+        u = torch.rand(bs * iw * mc, self.num_latents).to(self.device) # u ~ Unif[0,1]
         # v = torch.rand(self.batch_size * iw * mc, self.num_latents).to(self.device) # v ~ Unif[0,1]
         z = self.qlogits + torch.log(u) - torch.log1p(-u) # reparametrizing samples, appendix B
         z = z.gt(0.).type_as(z) # Bernoulli distributed w/ logits "px_logits". Denoted "b" in toy example from Backpropagation Through The Void.
@@ -46,18 +45,26 @@ class ToyModel(Template):
 
         # values from the stochastic layers (z, pz, qz) are returned
         # as a list where each index correspond to one stochastic layer
-        diagnostics = self._get_diagnostics(z, qz, pz)
+        diagnostics = self._get_diagnostics(x, qz, pz)
 
         return {'px': px, 'z': [z], 'qz': [qz], 'pz': [pz], 'qlogits': [self.qlogits], **diagnostics}
 
-    def _get_diagnostics(self, z, qz, pz):
-        # Hack for compatability with estimators
-        Hp = torch.tensor([torch.zeros_like(pz.entropy())])
-        Hp = Hp[:,None]
-        if isinstance(pz, PseudoCategorical):
-            usage = torch.zeros_like(Hp)
-        else:
-            usage = torch.zeros_like(Hp)
+    def sample_from_prior(self, N, from_optimal=False):
+        # Hack for compatability with run script
+        pz = Bernoulli(logits=self.prior_logits)
+        z = pz.sample()
+        px = Normal(0,1)
+        return {'px': None, 'z': z}
 
-        return {'Hp': [Hp], 'usage': [usage]}
+    @torch.no_grad()
+    def _get_diagnostics(self, x, qz, pz):
+        # Calculates loss used in plot for toy problem
+        thetas = torch.sigmoid(self.qlogits.detach())# .cpu().numpy()
+        plot_loss = thetas * (1 - x) ** 2 + (1 - thetas) * x **2 # Loss = E_p [ f(x) ], f(x) = (x-t)**2
+        plot_loss = plot_loss.mean()
+        print(plot_loss)
+        print(self.qlogits)
+        print("X = ", x)
+        return {'bernoulli_mse': plot_loss}
+
 
