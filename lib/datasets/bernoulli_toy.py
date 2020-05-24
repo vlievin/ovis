973daf71dcd189ba0fa05a321f3bd728751e04aa
Anderschri <47868390+Anderschri@users.noreply.github.com>
2020-05-24 21:38:35 +0200

    Update bernoulli_toy.py

--- lib/datasets/bernoulli_toy.py
+++ lib/datasets/bernoulli_toy.py
@@ -4,21 +4,22 @@ from torch.utils.data import Dataset
 
 
 class BernoulliToyDataset(Dataset):
-    def __init__(self, target, N=1, D=20, seed=42):
+    def __init__(self, target, N=1, bs=1):
         super().__init__()
         self.N = N
+        self.bs = bs
         self.target = target
         target = torch.Tensor(1, N)
         target.fill_(self.target)
-        self.data = [target]
+        self.data = [target] * bs
 
     def __len__(self):
-        return 1
+        return self.bs
 
     def __getitem__(self, item):
         return self.data[item]
 
 
-def get_bernoulli_toy_datasets(target=0.499, N=1, **kwargs):
-    train_dset = BernoulliToyDataset(target=target, N=N)
+def get_bernoulli_toy_datasets(target=0.499, N=1, bs=1):
+    train_dset = BernoulliToyDataset(target=target, N=N, bs=bs)
     return train_dset, train_dset, train_dset
