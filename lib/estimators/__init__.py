b565095dd6955c8ac16a72f4b1e5c5269f279cae
Anderschri <47868390+Anderschri@users.noreply.github.com>
2020-05-24 21:09:00 +0200

    Add Rebar and exact Bernoulli

--- lib/estimators/__init__.py
+++ lib/estimators/__init__.py
@@ -1,6 +1,7 @@
 from .reinforce import Reinforce, Vimco, VimcoPlus
-from .relax import Relax
+from .relax import Relax, Rebar
 from .tvo import ThermoVariationalObjective
 from .vi import VariationalInference, PathwiseIWAE, PathwiseVAE, SafeVariationalInference
 from .wakesleep import WakeSleep, WakeWake
 from .air import AirReinforce
+from .exact_bernoulli_toy import ExactGradientBernoulli
