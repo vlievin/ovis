96f727c39b4531c2887f6d7e3fd89d086137d2c5
Valentin Liévin <valentin.lievin@gmail.com>
2020-07-10 12:28:23 +0200

    proper wrapping using functools + docstring

--- ovis/training/ops.py
+++ ovis/training/ops.py
@@ -1,10 +1,13 @@
+from functools import wraps
 from time import time
 
 import torch
 
 
-def append_ellapsed_time(func):
+def append_elapsed_time(func):
     """append the elapsed time to the diagnostics"""
+
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time()
         diagnostics = func(*args, **kwargs)
@@ -14,8 +17,10 @@ def append_ellapsed_time(func):
     return wrapper
 
 
-@append_ellapsed_time
+@append_elapsed_time
 def training_step(x, model, estimator, optimizers, **config):
+    """Perform one optimization step of the `model` given the mini-batch of observations `x`,
+    the gradient estimator/evaluator `estimator` and the list of optimizers `optimizers`"""
     loss, diagnostics, output = estimator(model, x, backward=True, **config)
 
     [o.step() for o in optimizers]
@@ -25,7 +30,8 @@ def training_step(x, model, estimator, optimizers, **config):
 
 
 @torch.no_grad()
-@append_ellapsed_time
+@append_elapsed_time
 def test_step(x, model, estimator, **config):
+    """Test the `model` given the mini-batch of observations `x` and the gradient estimator/evaluator `estimator`"""
     loss, diagnostics, output = estimator(model, x, backward=False, **config)
     return diagnostics
